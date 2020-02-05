bfe087ac46c45a4c37f5fc3c2461e3d60f51cf27
hs_ikawa.yuki <ikawa.yuki@heartsoft.co.jp>
2020-02-05 13:52:43 +0900

    add junit sample

--- src/test/java/jp/co/heartsoft/ikaqa/controller/IkaQAControllerTest.java
+++ src/test/java/jp/co/heartsoft/ikaqa/controller/IkaQAControllerTest.java
@@ -1,11 +1,25 @@
 package jp.co.heartsoft.ikaqa.controller;
 
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class IkaQAControllerTest {
+    private IkaQAController ikaQAController;
+    @BeforeEach
+    void setUp() {
+        ikaQAController = new IkaQAController();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
     @Test
     public void executeSlashCommand() {
-        assertEquals(true, true);
+        assertEquals("call postMessage", ikaQAController.executeSlashCommand());
     }
+
+
 }
