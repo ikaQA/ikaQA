7b2524c7c90822d858e006d0cd4ae16b2f12ef2a
heartsoft-ueno-takayuki <ueno.takayuki@heartsoft.co.jp>
2020-02-13 14:24:00 +0900

    add another test

---
+++ src/test/java/jp/co/heartsoft/ikaqa/controller/ikaQAApplicationTest.java
@@ -0,0 +1,20 @@
+package jp.co.heartsoft.ikaqa.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class ikaQAApplicationTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+        org.junit.jupiter.api.Assertions.assertEquals(0, 1);
+    }
+}
